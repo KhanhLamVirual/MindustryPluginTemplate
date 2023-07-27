bafa2e269d84cb374d599ba39adfcb0cbe96720c
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-07-27 09:03:20 +0700

    Update Main.java

--- src/utilsplugin/Main.java
+++ src/utilsplugin/Main.java
@@ -14,9 +14,7 @@ public class Main extends Plugin{
   @Override
   public void init(){
     final String TOKEN = "MTEzMzkzOTk0OTAyOTMwNjQ5OA.GcxXBn.bIilpkmiSSfblTS7vYpmq8dj__n3U0H4jwxshA";
-    JDABuilder bot = JDABuilder.createDefault(TOKEN)
-      .setActivity(Activity.playing("vndustry.ddnd.net"))
-      .build();
+    JDABuilder bot = JDABuilder.createDefault(TOKEN).build()
     Events.on(PlayerChatEvent.class, event -> {
       Player player = event.player;
       String message = event.message;
