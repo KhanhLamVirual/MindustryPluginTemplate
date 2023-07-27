3e8b35183f2d27974f29f0f22824eb7934957b6f
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-07-27 07:57:26 +0700

    Update Main.java

--- src/utilsplugin/Main.java
+++ src/utilsplugin/Main.java
@@ -1,11 +1,14 @@
 package utilsplugin;
 
 import net.dv8tion.jda.api.*;
+import mindustry.gen.*;
 import mindustry.game.EventType.*;
 
 public class Main extends Plugin{
   @Override
   public void init(){
-    
+    Events.on(PlayerChatEvent.class, event -> {
+      Player player = event.player;
+    }
   }
 }
