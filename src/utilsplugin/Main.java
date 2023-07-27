4b6a247a491852256541d6e1669e4da623619e17
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-07-27 11:38:48 +0700

    Update Main.java

--- src/utilsplugin/Main.java
+++ src/utilsplugin/Main.java
@@ -14,13 +14,13 @@ public class Main extends Plugin{
   @Override
   public void init(){
     final String TOKEN = "MTEzMzkzOTk0OTAyOTMwNjQ5OA.GcxXBn.bIilpkmiSSfblTS7vYpmq8dj__n3U0H4jwxshA";
-    
+    final int ChannelID = 1131854700669575198
     DiscordClient client = DiscordClient.create(TOKEN);
     Events.on(PlayerChatEvent.class, event -> {
       Player player = event.player;
       String playerchat = event.message;
       GatewayDiscordClient gateway = client.login().block();
-      MessageChannel channel = gateway.getChannelById("1131854700669575198").ofType(TextChannel.class).block();
+      MessageChannel channel = gateway.getChannelById(ChannelID).ofType(TextChannel.class).block();
       channel.createMessage("[" + player + "] > " + playerchat).block();
       gateway.logout().block();
     });
