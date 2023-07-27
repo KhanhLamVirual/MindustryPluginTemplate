317ced2c530d0ecfdbe698ede4250381f2baea70
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-07-27 11:22:03 +0700

    Update Main.java

--- src/utilsplugin/Main.java
+++ src/utilsplugin/Main.java
@@ -1,7 +1,7 @@
 package utilsplugin;
 
-import net.dv8tion.jda.api.*;
-import net.dv8tion.jda.api.entities.*;
+import discord4j.core.*;
+import discord4j.core.object.entity.channel.*;
 
 import arc.*;
 import arc.util.*;
@@ -14,15 +14,14 @@ public class Main extends Plugin{
   @Override
   public void init(){
     final String TOKEN = "MTEzMzkzOTk0OTAyOTMwNjQ5OA.GcxXBn.bIilpkmiSSfblTS7vYpmq8dj__n3U0H4jwxshA";
-    JDA bot = JDABuilder.createDefault(TOKEN)
-      .setActivity(Activity.playing("vndustry.ddns.net"))
-      .build().awaitReady();
+    DiscordClient bot = DiscordClient.create(TOKEN);
     Events.on(PlayerChatEvent.class, event -> {
       Player player = event.player;
       String playerchat = event.message;
-      
-      TextChannel channel = bot.getTextChannelById("1131854700669575198");
-      channel.sendMessage("[" + player + "] > " + playerchat).queue();
+      GatewayDiscordClient gateway = client.login().block();
+      MessageChannel channel = gateway.getChannelById("1131854700669575198").ofType(MessageChannel.class).block();
+      channel.createMessage("[" + player + "] > " + playerchat).block();
+      gateway.logout().block();
     });
   }
 }
