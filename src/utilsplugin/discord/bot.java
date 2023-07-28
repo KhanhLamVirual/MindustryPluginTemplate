d0e58afaf5a005dae99a65bcfe973ad270626fe8
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-07-28 09:30:34 +0700

    Update bot.java

--- src/utilsplugin/discord/bot.java
+++ src/utilsplugin/discord/bot.java
@@ -9,15 +9,14 @@ import discord4j.core.object.entity.channel.*;
 public class Bot {
   public static void connect() {
     DiscordClient client = DiscordClient.create(TOKEN);
-  }
-  public static void sendchat(String[] message) {
     GatewayDiscordClient gateway = client.login().block();
+  }
+  public static void sendchat(String message) {
     MessageChannel channel = gateway.getChannelById(Snowflake.of(1131854700669575198L)).ofType(TextChannel.class).block();
     EmbedCreateSpec chatembed = EmbedCreateSpec.builder()
       .color(Color.DARK_GRAY)
       .title(message)
       .build();
     channel.createMessage(chatembed).block();
-    gateway.logout().block();
   }
 }
