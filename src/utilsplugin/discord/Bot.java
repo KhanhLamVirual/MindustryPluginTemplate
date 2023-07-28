11fa19fab3242293184f8d72344410e1aecb05ee
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-07-28 09:52:44 +0700

    Update and rename bot.java to Bot.java

--- src/utilsplugin/discord/Bot.java
+++ src/utilsplugin/discord/Bot.java
@@ -7,12 +7,19 @@ import discord4j.core.spec.*;
 import discord4j.core.object.entity.channel.*;
 
 public class Bot {
+
+  private static MessageChannel channel;
+
+  
   public static void connect() {
     DiscordClient client = DiscordClient.create(TOKEN);
     GatewayDiscordClient gateway = client.login().block();
+    channel = gateway.getChannelById(Snowflake.of(1131854700669575198L)).ofType(TextChannel.class).block();
+    System.out.println("Bot Has Connected");
+    
   }
-  public static void sendchat(String message) {
-    MessageChannel channel = gateway.getChannelById(Snowflake.of(1131854700669575198L)).ofType(TextChannel.class).block();
+  public static void sendmessage(String message) {
+    
     EmbedCreateSpec chatembed = EmbedCreateSpec.builder()
       .color(Color.DARK_GRAY)
       .title(message)
